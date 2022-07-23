313aae12d98074bbfdc91efd040f67bfbb3acb17
Yousef Shaban <yousefshaban.eg@gmail.com>
2022-07-23 20:41:39 +0200

    inject.

---
+++ lib/injection_container.dart
@@ -0,0 +1,60 @@
+import 'package:clean_architecture/core/network/network_info.dart';
+import 'package:clean_architecture/features/posts/data/datasources/post_local_datasource.dart';
+import 'package:clean_architecture/features/posts/data/datasources/post_remote_datasource.dart';
+import 'package:clean_architecture/features/posts/data/repositories/post_repository_impl.dart';
+import 'package:clean_architecture/features/posts/domain/repositories/posts_repository.dart';
+import 'package:clean_architecture/features/posts/domain/usecases/add_post.dart';
+import 'package:clean_architecture/features/posts/domain/usecases/delete_post.dart';
+import 'package:clean_architecture/features/posts/domain/usecases/get_all_posts.dart';
+import 'package:clean_architecture/features/posts/domain/usecases/update_post.dart';
+import 'package:clean_architecture/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:clean_architecture/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Posts
+
+  // * Bloc
+
+  sl.registerFactory(() => PostsBloc(getAllPosts: sl()));
+  sl.registerFactory(() => AddDeleteUpdatePostBloc(
+        addPost: sl(),
+        deletePost: sl(),
+        updatePost: sl(),
+      ));
+
+  // * UseCases
+
+  sl.registerLazySingleton(() => GetAllPostsUseCase(sl()));
+  sl.registerLazySingleton(() => AddPostUseCase(sl()));
+  sl.registerLazySingleton(() => DeletePostUseCase(sl()));
+  sl.registerLazySingleton(() => UpdatePostUseCase(sl()));
+
+  // * Repositories
+
+  sl.registerLazySingleton<PostsRepository>(() => PostsRepositoryImpl(
+      remoteDataSource: sl(), localDataSource: sl(), networkInfo: sl()));
+
+  // * Data Sources
+
+  sl.registerLazySingleton<PostRemoteDataSource>(
+    () => PostRemoteDataSourceImpl(client: sl()),
+  );
+  sl.registerLazySingleton<PostLocalDataSource>(
+    () => PostLocalDataSourceImpl(sharedPreferences: sl()),
+  );
+
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
