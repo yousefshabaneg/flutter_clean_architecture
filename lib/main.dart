5e428df64e3913d3e2c423f4571cb84a291c0a3e
Yousef Shaban <yousefshaban.eg@gmail.com>
2022-07-23 20:41:07 +0200

    mplement Dependency Injection using GET_IT package

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
+import 'package:clean_architecture/features/posts/presentation/bloc/posts/posts_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+import 'features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => di.sl<PostsBloc>()),
+          BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+        ],
+        child: MaterialApp(
+          title: 'Material App',
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Material App Bar'),
+            ),
+            body: const Center(
+              child: Text('Hello World'),
+            ),
+          ),
+        ));
   }
 }
