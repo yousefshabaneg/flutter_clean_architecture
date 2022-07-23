2817a9339c04ac7bb6d9726793bfb76fdabdbf06
Yousef Shaban <yousefshaban.eg@gmail.com>
2022-07-23 03:22:55 +0200

    Data Layer.

---
+++ lib/features/posts/data/datasources/post_local_datasource.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:clean_architecture/core/error/exceptions.dart';
+import 'package:clean_architecture/features/posts/data/models/post_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const cachedPosts = "CACHED_POSTS";
+
+abstract class PostLocalDataSource {
+  Future<List<PostModel>> getCachedPosts();
+  Future<Unit> cachePosts(List<PostModel> postModels);
+}
+
+class PostLocalDataSourceImpl extends PostLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  PostLocalDataSourceImpl({required this.sharedPreferences});
+  @override
+  Future<Unit> cachePosts(List<PostModel> postModels) {
+    List postModelsToJson = postModels
+        .map<Map<String, dynamic>>((postModel) => postModel.toJson())
+        .toList();
+    sharedPreferences.setString(cachedPosts, json.encode(postModelsToJson));
+    return Future.value(unit);
+  }
+
+  @override
+  Future<List<PostModel>> getCachedPosts() {
+    final jsonString = sharedPreferences.getString(cachedPosts);
+    if (jsonString != null) {
+      List decodeJsonData = json.decode(jsonString);
+      List<PostModel> jsonToPostModels = decodeJsonData
+          .map<PostModel>((jsonPostModel) => PostModel.fromJson(jsonPostModel))
+          .toList();
+      return Future.value(jsonToPostModels);
+    } else {
+      throw EmptyCacheException();
+    }
+  }
+}
---
+++ lib/features/posts/data/repositories/post_repository_impl.dart
@@ -0,0 +1,83 @@
+import 'package:clean_architecture/core/error/exceptions.dart';
+import 'package:clean_architecture/core/network/network_info.dart';
+import 'package:clean_architecture/features/posts/data/models/post_model.dart';
+import 'package:clean_architecture/features/posts/domain/entities/post.dart';
+import 'package:clean_architecture/core/error/failures.dart';
+import 'package:clean_architecture/features/posts/domain/repositories/posts_repository.dart';
+import 'package:dartz/dartz.dart';
+
+import '../datasources/post_local_datasource.dart';
+import '../datasources/post_remote_datasource.dart';
+
+typedef DeleteOrUpdateOrAddPost = Future<Unit> Function();
+
+class PostsRepositoryImpl extends PostsRepository {
+  final PostRemoteDataSource remoteDataSource;
+  final PostLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  PostsRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+
+  @override
+  Future<Either<Failure, List<Post>>> getAllPosts() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePosts = await remoteDataSource.getAllPosts();
+        localDataSource.cachePosts(remotePosts);
+        return Right(remotePosts);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final cachedPosts = await localDataSource.getCachedPosts();
+        return Right(cachedPosts);
+      } on EmptyCacheException {
+        return Left(EmptyCacheFailure());
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> addPost(Post post) async {
+    final postModel =
+        PostModel(id: post.id, title: post.title, body: post.body);
+    return await _getMessage(
+      () => remoteDataSource.addPost(postModel),
+    );
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deletePost(int id) async {
+    return await _getMessage(
+      () => remoteDataSource.deletePost(id),
+    );
+  }
+
+  @override
+  Future<Either<Failure, Unit>> updatePost(Post post) async {
+    final postModel =
+        PostModel(id: post.id, title: post.title, body: post.body);
+    return await _getMessage(
+      () => remoteDataSource.updatePost(postModel),
+    );
+  }
+
+  Future<Either<Failure, Unit>> _getMessage(
+      DeleteOrUpdateOrAddPost deleteOrUpdateOrAddPost) async {
+    if (await networkInfo.isConnected) {
+      try {
+        await deleteOrUpdateOrAddPost();
+        return const Right(unit);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(OfflineFailure());
+    }
+  }
+}
